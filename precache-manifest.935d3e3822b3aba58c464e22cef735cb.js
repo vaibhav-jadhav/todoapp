8decbe7441393934f6c16c068d2a7a8bcd871b2a
vaibhav jadhav <vaibhav.jadhav.csm@gmail.com>
2020-12-09 23:23:18 +0530

    Updates

---
+++ precache-manifest.935d3e3822b3aba58c464e22cef735cb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3f6716c14a293997348c6e579a7d9b",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "0e4a39fa6173772a41d5",
+    "url": "/todoapp/static/css/2.f520119d.chunk.css"
+  },
+  {
+    "revision": "29191bd56d5222e359a9",
+    "url": "/todoapp/static/css/main.7d8716b7.chunk.css"
+  },
+  {
+    "revision": "0e4a39fa6173772a41d5",
+    "url": "/todoapp/static/js/2.cd37e0a5.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/todoapp/static/js/2.cd37e0a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29191bd56d5222e359a9",
+    "url": "/todoapp/static/js/main.298268cb.chunk.js"
+  },
+  {
+    "revision": "4eebab13957f3044ae12",
+    "url": "/todoapp/static/js/runtime-main.7fcb8c6f.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/todoapp/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/todoapp/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/todoapp/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/todoapp/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/todoapp/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
